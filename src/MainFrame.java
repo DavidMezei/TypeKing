23c4655655088db50af3f97d6de4753397365e33
David <davidmezei@hotmail.com>
2021-03-28 23:21:24 +0300

    Added a simple frame

---
+++ src/MainFrame.java
@@ -0,0 +1,42 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class MainFrame extends JFrame {
+    private JTextPane textPane;
+    private JTextField textField;
+
+    MainFrame() {
+        setLayout(new BorderLayout());
+        setTitle("TypeKing");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+        setBounds(1700,700,700,700);
+
+        textPane = new JTextPane();
+        setTextPane();
+        textField = new JTextField();
+        setTextField();
+
+        getContentPane().add(textPane,BorderLayout.CENTER);
+        getContentPane().add(textField,BorderLayout.NORTH);
+        getContentPane().setBackground(Color.black);
+        revalidate();
+
+    }
+
+    void setTextPane(){
+        textPane.setText("This is the text that I want to write here!");
+        textPane.setSize(new Dimension(100,100));
+        textPane.setBounds(200,200,200,200);
+        textPane.setBackground(Color.red);
+        textPane.revalidate();
+        textPane.repaint();
+    }
+
+    void setTextField(){
+        textField.setText("Start to type as fast you can!");
+        textField.setBounds(100,100,100,100);
+        textField.setBackground(Color.green);
+    }
+
+}
