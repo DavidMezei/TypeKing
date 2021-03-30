6f1f92e364d0d55e654e349a58b666a36b0ae86d
David <davidmezei@hotmail.com>
2021-03-30 17:55:55 +0300

    added frame and panel setups

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -4,39 +4,65 @@ import java.awt.*;
 public class MainFrame extends JFrame {
     private JTextPane textPane;
     private JTextField textField;
+    private Color frameBackgroundColor=Color.black;
 
     MainFrame() {
-        setLayout(new BorderLayout());
+        setUpFrame();
+
+        textPane = new JTextPane();
+        setUpTextPane();
+        textField = new JTextField();
+        setUpTextField();
+
+        JPanel panelTextPane = new JPanel();
+        setUpPanel(panelTextPane,textPane);
+        JPanel panelTextField = new JPanel();
+        setUpPanel(panelTextField,textField);
+
+        addComponentsToContentPane(panelTextPane,panelTextField);
+
+        revalidate();
+        pack();
+    }
+
+    void addComponentsToContentPane(JPanel panelTextPane, JPanel panelTextField){
+        getContentPane().add(Box.createRigidArea(new Dimension(0,50)));
+        getContentPane().add(panelTextPane);
+        getContentPane().add(Box.createRigidArea(new Dimension(0,25)));
+        getContentPane().add(panelTextField);
+        getContentPane().add(Box.createRigidArea(new Dimension(0,50)));
+    }
+
+    void setUpPanel(JPanel panel, Component comp){
+        panel.setBackground(getContentPane().getBackground());
+        panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
+        panel.add(Box.createRigidArea(new Dimension(50,0)));
+        panel.add(comp);
+        panel.add(Box.createRigidArea(new Dimension(50,0)));
+
+    }
+
+    void setUpTextPane(){
+        textPane.setText("This is the text that I want to write here!");
+        textPane.setMaximumSize(new Dimension(1000,200));
+        textPane.setFont(new Font("TimeRoman",Font.PLAIN,40));
+        textPane.setBackground(Color.yellow);
+    }
+
+    void setUpTextField(){
+        textField.setText("Start to type as fast as you can!");
+        textField.setMaximumSize(new Dimension(2000,60));
+        textField.setFont(new Font("TimesRoma",Font.PLAIN,40));
+        textField.setBackground(Color.green);
+    }
+
+    void setUpFrame(){
+        setLayout(new BoxLayout(getContentPane(), BoxLayout.PAGE_AXIS));
         setTitle("TypeKing");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
-        setBounds(1700,700,700,700);
-
-        textPane = new JTextPane();
-        setTextPane();
-        textField = new JTextField();
-        setTextField();
-
-        getContentPane().add(textPane,BorderLayout.CENTER);
-        getContentPane().add(textField,BorderLayout.NORTH);
-        getContentPane().setBackground(Color.black);
-        revalidate();
-
-    }
-
-    void setTextPane(){
-        textPane.setText("This is the text that I want to write here!");
-        textPane.setSize(new Dimension(100,100));
-        textPane.setBounds(200,200,200,200);
-        textPane.setBackground(Color.blue);
-        textPane.revalidate();
-        textPane.repaint();
-    }
-
-    void setTextField(){
-        textField.setText("Start to type as fast you can!");
-        textField.setBounds(100,100,100,100);
-        textField.setBackground(Color.green);
+        setBounds(600,200,700,700);
+        getContentPane().setBackground(frameBackgroundColor);
     }
 
 }
