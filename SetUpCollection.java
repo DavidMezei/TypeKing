900f0bd457c585518ab5f9dc2d94458532c26f20
David <davidmezei@hotmail.com>
2021-04-04 22:38:06 +0300

    Added setUpLabel function

--- SetUpCollection.java
+++ SetUpCollection.java
@@ -8,7 +8,7 @@ public class SetUpCollection {
         this.mainFrame = mainFrame;
     }
 
-    public  void setUpPanel(JPanel panel, Component component){
+    public void setUpPanel(JPanel panel, Component component){
         panel.setBackground(mainFrame.frameBackgroundColor);
         panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
         panel.add(Box.createRigidArea(new Dimension(50,0)));
@@ -16,7 +16,7 @@ public class SetUpCollection {
         panel.add(Box.createRigidArea(new Dimension(50,0)));
     }
 
-    public  void setUpTextPane(JTextPane textPane){
+    public void setUpTextPane(JTextPane textPane){
         textPane.setText(FileReader.getRandomText());
         textPane.setMaximumSize(new Dimension(600,300));
         textPane.setFont(new Font(mainFrame.defaultFont,Font.PLAIN,40));
@@ -26,7 +26,7 @@ public class SetUpCollection {
         textPane.setForeground(mainFrame.textColor);
     }
 
-    public  void setUpTextField(JTextField textField){
+    public void setUpTextField(JTextField textField){
         textField.setText("Just start to type!");
         textField.setMaximumSize(new Dimension(600,60));
         textField.setFont(new Font(mainFrame.defaultFont,Font.PLAIN,40));
@@ -34,7 +34,7 @@ public class SetUpCollection {
         textField.setForeground(Color.green.darker());
    }
 
-    public  void setUpFrame(JFrame frame){
+    public void setUpFrame(JFrame frame){
         frame.setLayout(new BoxLayout(frame.getContentPane(), BoxLayout.PAGE_AXIS));
         frame.setTitle("TypeKing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -44,4 +44,9 @@ public class SetUpCollection {
         frame.setLocationRelativeTo(null);
     }
 
+    public void setUpLabel(JLabel label){
+        label.setMaximumSize(new Dimension(200,70));
+        label.setFont(new Font(mainFrame.defaultFont,Font.PLAIN,50));
+        label.setForeground(mainFrame.labelColor);
+    }
 }
