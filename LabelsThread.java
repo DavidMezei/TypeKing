7535d9b07b078f3fe33d536a6953e9edcaf80903
David <davidmezei@hotmail.com>
2021-04-17 21:31:55 +0300

    Solved the bug which set up a negativ value to labels when start time is 13:59 and current time is 14:01

--- LabelsThread.java
+++ LabelsThread.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 import java.time.LocalTime;
 
 public class LabelsThread extends Thread {
-    static boolean stopThread = false;
+    private boolean stopThread = false;
     private JLabel labelWPM;
     private JLabel labelElapsedTime;
     private KeyProcessor keyProcessor;
@@ -20,17 +20,18 @@ public class LabelsThread extends Thread {
     @Override
     public void run() {
         LocalTime startThreadTime = LocalTime.now();
+        int elapsedTimeInSeconds;
+        int startThreadTimeInSeconds = startThreadTime.getMinute() * 60 + startThreadTime.getSecond();
         ThreadSleep.sleep(1000);
         labelElapsedTime.setText("Time: 0:01");
         ThreadSleep.sleep(1000);
-
-        int startThreadTimeInSeconds = startThreadTime.getMinute() * 60 + startThreadTime.getSecond();
         while (!stopThread) {
             LocalTime currentTime = LocalTime.now();
             int currentTimeInSeconds = currentTime.getMinute() * 60 + currentTime.getSecond();
-            if (currentTimeInSeconds < startThreadTimeInSeconds)
-                startThreadTimeInSeconds = -(60 - startThreadTimeInSeconds);
-            int elapsedTimeInSeconds = currentTimeInSeconds - startThreadTimeInSeconds;
+            if (currentTimeInSeconds > startThreadTimeInSeconds)
+                elapsedTimeInSeconds = currentTimeInSeconds - startThreadTimeInSeconds;
+            else
+                elapsedTimeInSeconds = 3600 - startThreadTimeInSeconds + currentTimeInSeconds; //if startTime=13:59 and currentTime=14:01
             double elapsedTimeInMinutes = elapsedTimeInSeconds / 60.0;
             double typedWords = keyProcessor.getCaretIndex() / 5.0;
             int WPM = (int) (typedWords / elapsedTimeInMinutes);
