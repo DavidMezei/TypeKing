e1a3262596a06c5b2bb3f90109214b923811ab4a
David <davidmezei@hotmail.com>
2021-04-17 21:34:17 +0300

    Refactored the code

--- SetUpCollection.java
+++ SetUpCollection.java
@@ -9,7 +9,7 @@ public class SetUpCollection {
     }
 
     public void setUpPanel(JPanel panel){
-        panel.setBackground(mainFrame.frameBackgroundColor);
+        panel.setBackground(mainFrame.getFrameBackgroundColor());
         panel.setLayout(new BoxLayout(panel, BoxLayout.LINE_AXIS));
         panel.add(Box.createRigidArea(new Dimension(50,0)));
     }
@@ -19,22 +19,28 @@ public class SetUpCollection {
         panel.add(Box.createRigidArea(new Dimension(50,0)));
     }
 
+    public void addComponentToPanelWithoutRigidArea(JPanel panel, Component component){
+        panel.add(component);
+    }
+
     public void setUpTextPane(JTextPane textPane){
         textPane.setText(FileReader.getRandomText());
-        textPane.setMaximumSize(new Dimension(600,300));
-        textPane.setFont(new Font(mainFrame.defaultFont,Font.PLAIN,40));
+        textPane.setMaximumSize(new Dimension(1200,200));
+        textPane.setFont(new Font(mainFrame.getDefaultFont(),Font.PLAIN,40));
         textPane.setEditable(false);
         textPane.setBorder(BorderFactory.createLineBorder(Color.cyan.darker(), 5));
-        textPane.setBackground(mainFrame.textBackgroundColor);
-        textPane.setForeground(mainFrame.textColor);
+        textPane.setBackground(mainFrame.getTextBackgroundColor());
+        textPane.setForeground(mainFrame.getTextColor());
+        textPane.setMargin(new Insets(10,10,10,10));
     }
 
     public void setUpTextField(JTextField textField){
         textField.setText("Just start to type!");
-        textField.setMaximumSize(new Dimension(600,60));
-        textField.setFont(new Font(mainFrame.defaultFont,Font.PLAIN,40));
-        textField.setBackground(mainFrame.textBackgroundColor);
+        textField.setMaximumSize(new Dimension(1200,60));
+        textField.setFont(new Font(mainFrame.getDefaultFont(),Font.PLAIN,40));
+        textField.setBackground(mainFrame.getTextBackgroundColor());
         textField.setForeground(Color.green.darker());
+        textField.requestFocusInWindow();
    }
 
     public void setUpFrame(JFrame frame){
@@ -42,20 +48,20 @@ public class SetUpCollection {
         frame.setTitle("TypeKing");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
-        frame.setBounds(600,200,700,700);
-        frame.getContentPane().setBackground(mainFrame.frameBackgroundColor);
+        frame.setBounds(600,200,1000,700);
+        frame.getContentPane().setBackground(mainFrame.getFrameBackgroundColor());
         frame.setLocationRelativeTo(null);
     }
 
     public void setUpLabel(JLabel label){
-        label.setMaximumSize(new Dimension(200,70));
-        label.setFont(new Font(mainFrame.defaultFont,Font.PLAIN,50));
-        label.setForeground(mainFrame.labelColor);
+        label.setMaximumSize(new Dimension(400,70));
+        label.setFont(new Font(mainFrame.getDefaultFont(),Font.PLAIN,50));
+        label.setForeground(mainFrame.getLabelColor());
     }
 
     public void setUpButton(JButton button){
         button.setMaximumSize(new Dimension(300,100));
-        button.setFont(new Font(mainFrame.defaultFont,Font.PLAIN,40));
+        button.setFont(new Font(mainFrame.getDefaultFont(),Font.PLAIN,45));
         button.setForeground(Color.orange);
         button.setBackground(Color.blue.darker());
     }
